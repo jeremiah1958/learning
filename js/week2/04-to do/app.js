5284bd84d343b2341a847f0f61846ac02cf7ee9b
jeremiah1958 <jeremiahbarakka@gmail.com>
2024-06-06 11:08:34 +0300

    to do list

---
+++ js/week2/04-to do/app.js
@@ -0,0 +1,31 @@
+const taskInput = document.getElementById("task-input");
+const addTaskBtn = document.getElementById("add-task-btn");
+const taskList = document.getElementById("task-list");
+addTaskBtn.addEventListener("click", function() {
+  const taskText = taskInput.value;
+  if (taskText.trim() !== "") {
+    const taskItem = document.createElement("li");
+    taskItem.innerText = taskText;
+    taskItem.addEventListener("click", function() {
+      taskItem.remove();
+    });
+    taskList.appendChild(taskItem);
+    taskInput.value = "";
+  }
+});
+
+const taskinput = document.getElementById("task-input");
+const addtaskBtn = document.getElementById("add-task-btn");
+const tasklist = document.getElementById("task-list");
+addTaskBtn.addEventListener("click", function() {
+  const taskText = taskInput.value;
+  if (taskText.trim() !== "") {
+    const taskItem = document.createElement("li");
+    taskItem.innerText = taskText;
+    taskItem.addEventListener("click", function() {
+      taskItem.remove();
+    });
+    taskList.appendChild(taskItem);
+    taskInput.value = "";
+  }
+});
