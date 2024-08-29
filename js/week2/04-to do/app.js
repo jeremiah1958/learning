e905d16d9326e8506fce503868da1982d8768d7f
Jeremiah Baraka <jeremiahbarakka@gmail.com>
2024-08-29 11:14:42 +0300

    wizard form

--- js/week2/04-to do/app.js
+++ js/week2/04-to do/app.js
@@ -1,6 +1,6 @@
 const taskInput = document.getElementById("task-input");
 const addTaskBtn = document.getElementById("add-task-btn");
-const taskList = document.getElementById("task-list");
+const taskList = document.getElementById("task-list"); 
 
 addTaskBtn.addEventListener("click", function() {
   const taskText = taskInput.value;
